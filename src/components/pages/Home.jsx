854357ab65b59dcb4e96de8e558aab0beee8fca0
octobrekids <chonrada.k@mail.kmutt.ac.th>
2019-11-29 21:34:03 +0700

    add card + home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {Row,Col} from 'antd'
+import RoomCard from './Home/RoomCard'
 
-export default function Home() {
-    return (
-        <div>
-            home
-        </div>
-    )
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+            <Row type="flex" justify="center">
+            <Col span={20}>
+                <span>search</span>
+                <Row><h1>Top Reservation</h1></Row>
+                <RoomCard />
+                </Col>
+                </Row>
+           
+            </div>
+        )
+    }
 }
