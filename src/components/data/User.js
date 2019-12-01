32963ec887219af084cf157e47c0157d9cc28f1f
yee2542 <yee2542gtl@gmail.com>
2019-12-01 23:15:09 +0700

    preparing User data components

---
+++ src/components/data/User.js
@@ -0,0 +1,28 @@
+class User {
+    constructor() {
+        this.user = {}
+        this.auth = [
+            { email: 'contact@korboon.com', password: '0' }
+        ]
+    }
+
+    login(email = '', password = '') {
+        const valid = this.auth.some(e => e.email === email)
+        if (!valid) return false
+        if (valid.password !== password) return false
+        localStorage.setItem('email', valid.email)
+    }
+
+    logout() {
+        localStorage.removeItem('email')
+    }
+
+    auth = () => {
+        const email = localStorage.getItem('email')
+        if (!email) return false
+        return true
+    }
+}
+
+const user = new User()
+export default user
