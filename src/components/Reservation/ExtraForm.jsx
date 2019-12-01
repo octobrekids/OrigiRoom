60b3e54ab6f824601729d11906aba1eee1b5fe43
octobrekids <chonrada.k@mail.kmutt.ac.th>
2019-12-01 19:36:34 +0700

    confirm register

--- src/components/Reservation/ExtraForm.jsx
+++ src/components/Reservation/ExtraForm.jsx
@@ -23,21 +23,21 @@ class ExtraForm extends Component {
     equipment: []
   };
 
-  handleSubmit = () => new Promise((resolve, reject) => resolve(this.state));
+  //handleSubmit = () => new Promise((resolve, reject) => resolve(this.state));
   // new Promise((resolve, reject) => {
   //   this.props.form.validateFields((err, values) => {
   //     if (err) return reject(err);
   //     console.log("Received values of form: ", values);
   //     resolve({ stage: 2, ...values });
   //   });
-  // });
+  //});
 
-  handleStatus = () => {
+  //handleStatus = () => {
     // this.props.form.validateFields((err, values) => {
     //   if (err) return this.props.onType({ status: false, values });
     //   return this.props.onType({ status: true, values });
     // });
-  };
+  //};
 
   onCapacity = value => {
     console.log(value);
--- src/components/pages/SearchRoom.jsx
+++ src/components/pages/SearchRoom.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Form,Row,Col,Button,Icon } from 'antd'
+
+
 
 export default function SearchRoom() {
+    const { getFieldDecorator } = this.props.form;
     return (
         <div>
-
-searchRoom
+        <Form.Item label={Field1}>
+        {getFieldDecorator('field1', {
+          rules: [
+            {
+              required: true,
+              message: 'Input something!',
+            },
+          ],
+        })(<Input placeholder="placeholder" />)}
+      </Form.Item>
         </div>
     )
 }
